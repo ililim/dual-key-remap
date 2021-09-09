690997dbc8db1988dd45ec4d6daf2ed77e633f68
ililim <ililim@nomail.invalid>
2021-09-08 22:52:42 -0700

    Use hex for logging

--- remap.c
+++ remap.c
@@ -44,7 +44,6 @@ int prev_dbg_direction = 0;
 void log_input(int scan_code, int virt_code, int dir)
 {
     if (!g_debug) return;
-
     // To avoid duplicate logs, only log if key input changed
     if (prev_dbg_scan_code == prev_dbg_scan_code
         && prev_dbg_virt_code == virt_code
@@ -54,7 +53,7 @@ void log_input(int scan_code, int virt_code, int dir)
     prev_dbg_direction = dir;
 
     KEY_DEF * key = find_key_def_by_virt_code(virt_code);
-    printf("(input) %s %s [scan:%d virt:%d]\n",
+    printf("(input) %s %s [scan:0x%02x virt:0x%02x]\n",
         key ? key->name : "KEY",
         fmt_dir(dir),
         scan_code,
